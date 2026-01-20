5c4601d0df6d295a3281ea919159ed95008afe52
qweadzs <qweadzs@naver.com>
2026-01-20 21:14:47 +0900

    chore: 디스크컨트롤러 init

---
+++ src/programmers/디스크컨트롤러.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class 디스크컨트롤러 {
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][]{{0, 3}, {1, 9}, {3, 5}}));
+    }
+}
